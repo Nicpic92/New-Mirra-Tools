256be37d3fa191a321f3b1d4094d985a54b35741
Nicpic92 <nickolaspicardircm@gmail.com>
2025-11-14 09:39:33 -0500

    Update get-categorization-config.js

--- netlify/functions/get-categorization-config.js
+++ netlify/functions/get-categorization-config.js
@@ -1,17 +1,19 @@
+// --- START OF FILE get-categorization-config.js ---
+
 const { Pool } = require('pg');
 
 exports.handler = async function(event, context) {
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
     try {
-        // Query now joins through categories to get team info for each rule
+        // Query now joins through categories to get team info and L1 flag for each rule
         const editRulesQuery = `
-            SELECT r.edit_text, c.category_name, t.id as team_id, t.team_name
+            SELECT r.edit_text, c.category_name, t.id as team_id, t.team_name, c.send_to_l1_monitor
             FROM claim_edit_rules r
             JOIN claim_categories c ON r.category_id = c.id
             JOIN teams t ON c.team_id = t.id;
         `;
         const noteRulesQuery = `
-            SELECT r.note_keyword, c.category_name, t.id as team_id, t.team_name
+            SELECT r.note_keyword, c.category_name, t.id as team_id, t.team_name, c.send_to_l1_monitor
             FROM claim_note_rules r
             JOIN claim_categories c ON r.category_id = c.id
             JOIN teams t ON c.team_id = t.id;
@@ -42,3 +44,4 @@ exports.handler = async function(event, context) {
         await pool.end();
     }
 };
+// --- END OF FILE get-categorization-config.js ---
