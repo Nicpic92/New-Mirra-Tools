8d7dad710f664bbcfa26f350b336a3e2ba1ec6c8
Nicpic92 <nickolaspicardircm@gmail.com>
2025-11-15 12:35:10 -0500

    Update get-categorization-config.js

--- netlify/functions/get-categorization-config.js
+++ netlify/functions/get-categorization-config.js
@@ -1,17 +1,17 @@
 // --- START OF FILE get-categorization-config.js ---
 
-const pool = require('./database.js'); // THE FIX: Use the shared pool
+const pool = require('./database.js');
 
 exports.handler = async function(event, context) {
     try {
         const editRulesQuery = `
-            SELECT r.edit_text, c.category_name, t.id as team_id, t.team_name, c.send_to_l1_monitor
+            SELECT r.config_id, r.edit_text, c.category_name, t.id as team_id, t.team_name, c.send_to_l1_monitor
             FROM claim_edit_rules r
             JOIN claim_categories c ON r.category_id = c.id
             LEFT JOIN teams t ON c.team_id = t.id;
         `;
         const noteRulesQuery = `
-            SELECT r.note_keyword, c.category_name, t.id as team_id, t.team_name, c.send_to_l1_monitor
+            SELECT r.config_id, r.note_keyword, c.category_name, t.id as team_id, t.team_name, c.send_to_l1_monitor
             FROM claim_note_rules r
             JOIN claim_categories c ON r.category_id = c.id
             LEFT JOIN teams t ON c.team_id = t.id;
